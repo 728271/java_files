0604efd9437755bbf6685b6d30c91ae5ebec366f
728271 <148901737+728271@users.noreply.github.com>
2023-10-26 13:33:53 -0400

    Create Nested_for.java

---
+++ Nested_for.java
@@ -0,0 +1,28 @@
+class Main {
+public static void main(String[] args) {
+int hightemp = 5
+for (int i=-3; i<=highTemp/2;i++){
+  System.out.print((i*1.8+32)+" ");"
+//System.out.println((i*1.8+32) +"\t");//tab space
+//System.out.print((i*1.8+32) +"\n");//next line
+}  
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+
+System.out.println("+----+");
+for (int i=1; i<=3;i++){
+  System.out.println("\\    /");
+  System.out.println("/    \\");
+}
+    System.out.println("+----+");
+
+
+
+
+
+    
+  }
+}
